25aabac5ff8a7d0654ee628a5ac91ae372b7f04b
hemalwork15-create <hemalwork15@gmail.com>
2025-11-24 23:08:25 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -3,16 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DB_USER = os.getenv("MYSQL_USER", "user")
-DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "password")
-DB_HOST = os.getenv("MYSQL_HOST", "db")  # <-- Use container name
+DB_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
+DB_HOST = os.getenv("MYSQL_HOST", "db")
 DB_PORT = os.getenv("MYSQL_PORT", 3306)
 DB_NAME = os.getenv("MYSQL_DB", "edudocs")
 
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
 Base = declarative_base()
